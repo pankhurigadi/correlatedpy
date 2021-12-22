f64866e3f1c4d2a51a50686c09d9b23f72e37e9c
Omar Boufous <uceeobo@ucl.ac.uk>
2021-12-22 22:25:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+setup(
+    setup_requires=['setup.cfg'],
+    setup_cfg=True
+)
