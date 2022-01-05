35d67c9464f54cc5bf6207207e5e7e2b9381c848
Omar Boufous <uceeobo@ucl.ac.uk>
2022-01-06 00:14:12 +0100

    Update main.py

--- correlatepy/main.py
+++ correlatepy/main.py
@@ -1,12 +1,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from random import randrange
-import csv
 import random
-from math import sqrt
-from termcolor import colored
 import quantecon as qe
 from tqdm.notebook import tqdm
 from collections import Counter
-
