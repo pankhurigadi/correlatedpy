26b6d35daa45a443ca0cfc9cc7baaff4111aa72e
Omar Boufous <uceeobo@ucl.ac.uk>
2022-01-06 00:15:01 +0100

    Update main.py

--- correlatepy/main.py
+++ correlatepy/main.py
@@ -5,3 +5,18 @@ import random
 import quantecon as qe
 from tqdm.notebook import tqdm
 from collections import Counter
+
+
+## Chicken game
+epsilon = 0.02
+history = [(0,0)]
+
+P0 = Player(0, [0, 1], np.array([[0, 7], [2, 6]]), history, epsilon)
+P1 = Player(1, [0, 1], np.array([[0, 2], [7, 6]]), history, epsilon)
+
+G = Game(history, epsilon)
+
+G.add_player(P0)
+G.add_player(P1)
+
+G.runGame()
